4721c46bab6060312dbf43ebc22db3618a19018f
Noah MacGillivray <115374647+noahMacg@users.noreply.github.com>
2024-10-30 08:42:10 -0600

    Starting project

---
+++ Main.java
@@ -0,0 +1,35 @@
+/*
+This code is provided to give you a
+starting place. It should be modified.
+No further imports are needed.
+To earn full credit, you must also
+answer the following question:
+
+Q1: One of the goals of multi-threading
+is to minimize the resource usage, such
+as memory and processor cycles. In three
+sentences, explain how multi-threaded
+code accomplishes this goal. Consider
+writing about blocking on I/O, multicore 
+machines, how sluggish humans are,
+threads compared to processes, etcetera,
+and connect these issues to 
+multi-threading.
+
+*/
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println();
+		
+		
+		
+	}
+
+}
